8d96a377a2fd5aad7d993333d0c95c42b364541c
KorzhovAlexander <honeyiamgood@mail.ru>
2019-07-03 14:11:11 +0300

    2

--- src/main2.java
+++ src/main2.java
@@ -4,12 +4,24 @@ import java.util.Scanner;
 public class main2 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        byte a = Byte.parseByte(in.nextLine(),2);
-        byte b = Byte.parseByte(in.nextLine(),2);
 
-        int c = (a+b);
-        System.out.println(Integer.toBinaryString(c));
-        System.out.println(new BigInteger(String.valueOf(a)).bitCount());
-        System.out.println(new BigInteger(String.valueOf(b)).bitCount());
+        String a = in.nextLine();
+        String b = in.nextLine();
+        int i = (int)Math.ceil((float)a.length()/b.length());
+        int rez=0;
+            while (i!=0){
+                rez+=getRez(a,b);
+                a=removeCharAt(a);
+                i--;
+            }
+        System.out.println(new BigInteger(Integer.toBinaryString(rez),2).bitCount());
+    }
+
+    private static String removeCharAt(String s) {
+        return s.substring(0, 0) + s.substring(1);
+    }
+
+    private static int getRez(String a, String b) {
+        return (Integer.parseInt(a,2)+Integer.parseInt(b,2));
     }
 }
